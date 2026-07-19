744d291e1ed231db23e2cf8bfecd6a090c7a2226
Aaron <179076459+StochasticEagle@users.noreply.github.com>
2026-07-19 02:51:45 -0700

    Resolve Monitor type ambiguity

--- src/DisplayPilot.Display/Interfaces/IDdcController.cs
+++ src/DisplayPilot.Display/Interfaces/IDdcController.cs
@@ -3,6 +3,7 @@
 // See the LICENSE file in the project root for more information.
 
 using DisplayPilot.Display.Models;
+using Monitor = DisplayPilot.Display.Models.Monitor;
 
 namespace DisplayPilot.Display.Interfaces;
 
